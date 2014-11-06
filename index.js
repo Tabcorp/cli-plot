e9b71ba8adb8bb69dcb5bfab79d82c2087423772
Romain Prieto <romain.prieto@gmail.com>
2014-11-06 14:42:53 +1100

    Simplify code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
+var fixedQueue = require('fixedqueue').FixedQueue;
 var charm  = require('charm')();
 var babar  = require('babar');
 var split  = require('split');
-var queue  = require('./lib/queue');
 
 var cli = require('optimist')
   .usage('Plot values from stdin.\nUsage: $0 -w [width] -h [height]')
@@ -18,7 +20,33 @@ var width = count * 3;
 var height = cli.argv.h;
 
 var first = true;
-var data = queue.create(count);
+var queue = fixedQueue(count);
+for (var i = 0; i < count; ++i) queue.push(0);
+
+function readValue(chunk) {
+  var value = parseFloat(chunk);
+  if (!isNaN(value)) {
+    queue.enqueue(value);
+  }
+}
+
+function plot() {
+  var data = queue.map(function(value, index) {
+    return [index + 1, value];
+  });
+  var max = queue.reduce(function(acc, val) {
+    return (val > acc) ? val : acc;
+  }, 0);
+  var chart = babar(data, {
+    color: 'green',
+    width: width,
+    height: height,
+    yFractions: max > 10 ? 0 : 1
+  });
+  if (!first) charm.up(height + 2);
+  first = false;
+  console.log('\n' + chart + '\n');
+}
 
 charm.pipe(process.stdout);
 process.stdin.resume();
@@ -26,29 +54,9 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.pipe(split()).on('data', function(buffer) {
   readValue(buffer.toString());
+  plot();
 });
 
 process.stdin.on('end', function() {
   process.stdout.write('\n');
 });
-
-function readValue(chunk) {
-  var value = parseFloat(chunk);
-  if (!isNaN(value)) {
-    data.add(value);
-    plot();
-  }
-}
-
-function plot() {
-  var chart = babar(data.get(), {
-    color: 'green',
-    width: width,
-    height: height,
-    yFractions: data.max() > 10 ? 0 : 1
-  });
-  if (!first) charm.up(height + 2);
-  first = false;
-  // console.log('data', data.get().length, 'width', args.n * 3);
-  console.log('\n' + chart + '\n');
-}
